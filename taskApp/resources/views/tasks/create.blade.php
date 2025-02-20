59aed8633ee6d90c1faa8b58ba8d2109eee38118
Micah Kinnard <142631462+Micah-Kinnard@users.noreply.github.com>
2025-02-20 20:40:32 +0000

    finished blades without tailwind styling added

--- taskApp/resources/views/tasks/create.blade.php
+++ taskApp/resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 <div class="max-w-2xl mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">Create New Task</h1>
 
-    <form action="" method="POST">
+    <form action="{{ route('tasks.store') }}" method="POST">
         @csrf
 
         {{-- Task Name --}}
